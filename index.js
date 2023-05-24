0e1b2b863ca1d42f467a06e4ebf1e9a6431bcdb9
langesh <dev.langesh@gmail.com>
2023-05-24 22:05:28 +0530

    test 6

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  return res.json({ message: "hello" });
+  return res.json({ message: "hello langesh" });
 });
 
 app.listen(PORT, () => console.log(`server is listening on ${PORT}`));
