3bde683bd28309afc6738ca36e680bf77b3cad06
langesh <103199669+dev-langesh@users.noreply.github.com>
2023-06-03 10:29:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  return res.json({ message: "hello", name: "langesh" });
+  return res.json({ message: "hello", name: "langesh", version: "v2" });
 });
 
 app.listen(PORT, () => console.log(`server is listening on ${PORT}`));
